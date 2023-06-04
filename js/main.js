45cfbeee05b5b23c8aabde1fd9df6b338932e2c4
Victoria Delk <vdelkpictures@gmail.com>
2023-06-04 12:51:46 -0700

    Add map link to header, add info to popups, and fix pagination spacing

--- js/main.js
+++ js/main.js
@@ -5,162 +5,189 @@ let currentPage = null;
 
 // event listeners
 document
-	.getElementById("last-page")
-	.addEventListener("click", () => setStory(currentPage - 1));
+    .getElementById("last-page")
+    .addEventListener("click", () => setStory(currentPage - 1));
 document
-	.getElementById("next-page")
-	.addEventListener("click", () => setStory(currentPage + 1));
+    .getElementById("next-page")
+    .addEventListener("click", () => setStory(currentPage + 1));
 
 // use the variables
 const map = L.map("map").setView(mapOptions.center, mapOptions.zoom);
 map.addControl(new L.Control.Fullscreen());
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-	attribution:
-		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 let neighborhoodDataStruct = {};
 
 let neighborhoodData;
 fetch("static/neighborhoods.geojson")
-	.then((response) => response.json())
-	.then((data) => {
-		neighborhoodData = data;
-		neighborhoodData.features.forEach((feature) => {
-			neighborhoodDataStruct[feature.properties.name] = {
-				total: 0,
-				yesCount: 0,
-				responses: [],
-			};
-		});
-	});
+    .then((response) => response.json())
+    .then((data) => {
+        neighborhoodData = data;
+        neighborhoodData.features.forEach((feature) => {
+            neighborhoodDataStruct[feature.properties.name] = {
+                total: 0,
+                yesCount: 0,
+                responses: [],
+            };
+        });
+    });
 
 // Fetch CSV data
 Papa.parse(
-	"https://docs.google.com/spreadsheets/d/e/2PACX-1vTntSWZ4GHhgcLHHYOY5UH56ktQS_gO1MAT_rBWTFMpU40ZK5Bm95DiMTy3OgnSfXvCz8wL6xIw9GlN/pub?output=csv",
-	{
-		download: true,
-		header: true,
-		complete: function (results) {
-			const csvData = results.data;
-			csvData.forEach((item) => {
-				const point = turf.point([item.lng, item.lat]);
-				const response =
-					item[
-					"Did you feel supported in reaching your (college) goals in high school?"
-					];
-				neighborhoodData.features.forEach((feature) => {
-					if (turf.booleanPointInPolygon(point, feature)) {
-						neighborhoodDataStruct[feature.properties.name].total++;
-						if (response === "Yes") {
-							neighborhoodDataStruct[feature.properties.name]
-								.yesCount++;
-						}
-						neighborhoodDataStruct[
-							feature.properties.name
-						].responses.push(item);
-					}
-				});
-			});
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTntSWZ4GHhgcLHHYOY5UH56ktQS_gO1MAT_rBWTFMpU40ZK5Bm95DiMTy3OgnSfXvCz8wL6xIw9GlN/pub?output=csv",
+    {
+        download: true,
+        header: true,
+        complete: function (results) {
+            const csvData = results.data;
+            csvData.forEach((item) => {
+                const point = turf.point([item.lng, item.lat]);
+                const response =
+                    item[
+                        "Did you feel supported in reaching your (college) goals in high school?"
+                    ];
+                neighborhoodData.features.forEach((feature) => {
+                    if (turf.booleanPointInPolygon(point, feature)) {
+                        neighborhoodDataStruct[feature.properties.name].total++;
+                        if (response === "Yes") {
+                            neighborhoodDataStruct[feature.properties.name]
+                                .yesCount++;
+                        }
+                        neighborhoodDataStruct[
+                            feature.properties.name
+                        ].responses.push(item);
+                    }
+                });
+            });
 
-			let neighborhoods = L.geoJSON(neighborhoodData, {
-				style: function (feature) {
-					// Colors based on % of yes responses for Support in Reaching College Goals. Prob will change to something else later.
-					const total =
-						neighborhoodDataStruct[feature.properties.name].total;
-					const yesCount =
-						neighborhoodDataStruct[feature.properties.name]
-							.yesCount;
-					const yesPercentage = total === 0 ? 0 : yesCount / total;
-					let color;
-					if (total === 0) color = "none";
-					else if (yesPercentage < 0.25) color = "red";
-					else if (yesPercentage < 0.5) color = "yellow";
-					else if (yesPercentage < 0.75) color = "lightgreen";
-					else color = "purple";
-					return { color: color };
-				},
-				onEachFeature: function (feature, layer) {
-					const responses =
-						neighborhoodDataStruct[
-							feature.properties.name
-						].responses.join("<br>");
-					layer.bindPopup(
-						`<strong>${feature.properties.name}</strong><br>${responses}`
-					);
-					layer.on("click", () => {
-						stories =
-							neighborhoodDataStruct[feature.properties.name]
-								.responses;
-						document.getElementById(
-							"stories-content"
-						).style.display = "block";
-						document.getElementById(
-							"stories-placeholder"
-						).style.display = "none";
-						document.getElementById("page-max").innerHTML =
-							stories.length;
-						setStory(0);
-					});
-				},
-			}).addTo(map);
-		},
-	}
+            L.geoJSON(neighborhoodData, {
+                style: function (feature) {
+                    // Colors based on % of yes responses for Support in Reaching College Goals. Prob will change to something else later.
+                    const total =
+                        neighborhoodDataStruct[feature.properties.name].total;
+                    const yesCount =
+                        neighborhoodDataStruct[feature.properties.name]
+                            .yesCount;
+                    const yesPercentage = total === 0 ? 0 : yesCount / total;
+                    let color;
+                    if (total === 0) color = "none";
+                    else if (yesPercentage < 0.25) color = "red";
+                    else if (yesPercentage < 0.5) color = "yellow";
+                    else if (yesPercentage < 0.75) color = "lightgreen";
+                    else color = "purple";
+                    return { color: color };
+                },
+                onEachFeature: function (feature, layer) {
+                    layer.bindPopup(
+                        `<div class="popup"><strong>${
+                            feature.properties.name
+                        }</strong><span>Number of respondents: ${
+                            neighborhoodDataStruct[feature.properties.name]
+                                .total
+                        }</span><p>Did you feel supported in reaching your goals in high school?</p><ul><li><span class="label">Yes:</span>${getResponseCount(
+                            neighborhoodDataStruct,
+                            feature.properties.name,
+                            "Yes"
+                        )}</li><li><span class="label">Unsure:</span>${getResponseCount(
+                            neighborhoodDataStruct,
+                            feature.properties.name,
+                            "Unsure"
+                        )}</li><li><span class="label">No:</span>${getResponseCount(
+                            neighborhoodDataStruct,
+                            feature.properties.name,
+                            "No"
+                        )}</li></ul></div>`
+                    );
+                    layer.on("click", () => {
+                        stories =
+                            neighborhoodDataStruct[feature.properties.name]
+                                .responses;
+                        document.getElementById(
+                            "stories-content"
+                        ).style.display = "block";
+                        document.getElementById(
+                            "stories-placeholder"
+                        ).style.display = "none";
+                        document.getElementById("page-max").innerHTML =
+                            stories.length;
+                        setStory(0);
+                    });
+                },
+            }).addTo(map);
+        },
+    }
 );
 
 // Legend
-var legend = L.control({ position: "bottomright" });
+let legend = L.control({ position: "bottomright" });
 
 legend.onAdd = function (map) {
-	var div = L.DomUtil.create("div", "info legend"),
-		grades = [0, 0.25, 0.5, 0.75],
-		colors = ["red", "yellow", "lightgreen", "purple"];
+    let div = L.DomUtil.create("div", "info legend"),
+        grades = [0, 0.25, 0.5, 0.75],
+        colors = ["red", "yellow", "lightgreen", "purple"];
 
-	div.style.backgroundColor = "white";
-	div.style.padding = "10px";
-	div.style.borderRadius = "5px";
+    div.style.backgroundColor = "white";
+    div.style.padding = "10px";
+    div.style.borderRadius = "5px";
 
-	div.innerHTML = "Support in reaching college goals:<br>";
-	for (var i = 0; i < grades.length; i++) {
-		div.innerHTML +=
-			'<i style="background:' +
-			colors[i] +
-			'; width: 18px; height: 18px; float: left;"></i> ' +
-			grades[i] * 100 +
-			(grades[i + 1] ? "&ndash;" + grades[i + 1] * 100 + "%<br>" : "+%");
-	}
-	return div;
+    div.innerHTML = "Support in reaching college goals:<br>";
+    for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' +
+            colors[i] +
+            '; width: 18px; height: 18px; float: left;"></i> ' +
+            grades[i] * 100 +
+            (grades[i + 1] ? "&ndash;" + grades[i + 1] * 100 + "%<br>" : "+%");
+    }
+    return div;
 };
 
 legend.addTo(map);
 
 const setStory = (page) => {
-	currentPage = page;
-	if (page < 0 || page >= stories.length) {
-		return;
-	}
-	document.getElementById("page-num").innerHTML = currentPage + 1 + "/";
-	document.getElementById("college-prep-resources").innerHTML =
-		stories[page][
-		"Which college preparatory resources did your high school offer?"
-		];
-	document.getElementById("career-resources").innerHTML =
-		stories[page][
-		"During high school did you ever see a career counselor or attend a career event?"
-		];
-	document.getElementById("support").innerHTML =
-		stories[page][
-		"What do you remember about this support and how did that make you feel at the time?"
-		];
-	document.getElementById("post-grad").innerHTML =
-		stories[page][
-		"Please share how you felt that your high school education prepared you for the steps after high school graduation."
-		];
+    currentPage = page;
+    if (page < 0 || page >= stories.length) {
+        return;
+    }
+    document.getElementById("page-num").innerHTML = currentPage + 1 + "/";
+    document.getElementById("college-prep-resources").innerHTML =
+        stories[page][
+            "Which college preparatory resources did your high school offer?"
+        ];
+    document.getElementById("career-resources").innerHTML =
+        stories[page][
+            "During high school did you ever see a career counselor or attend a career event?"
+        ];
+    document.getElementById("support").innerHTML =
+        stories[page][
+            "What do you remember about this support and how did that make you feel at the time?"
+        ];
+    document.getElementById("post-grad").innerHTML =
+        stories[page][
+            "Please share how you felt that your high school education prepared you for the steps after high school graduation."
+        ];
 };
 
 // Clear story when click off of region
 map.on("click", () => {
-	document.getElementById("stories-content").style.display = "none";
-	document.getElementById("stories-placeholder").style.display = "flex";
+    document.getElementById("stories-content").style.display = "none";
+    document.getElementById("stories-placeholder").style.display = "flex";
 });
+
+const getResponseCount = (neighborhoodDataStruct, name, response) => {
+    let count = 0;
+    for (let i = 0; i < neighborhoodDataStruct[name].total; i++) {
+        if (
+            neighborhoodDataStruct[name].responses[i][
+                "Did you feel supported in reaching your (college) goals in high school?"
+            ] === response
+        ) {
+            count++;
+        }
+    }
+    return count;
+};
